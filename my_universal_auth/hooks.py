d73aae8aae117bc816b45513928e7bc9138343ad
tirathconstacloud-max <tirath.constacloud@gmail.com>
2026-02-17 14:59:01 +0530

    Add files via upload

---
+++ my_universal_auth/hooks.py
@@ -0,0 +1,15 @@
+app_name = "my_universal_auth"
+app_title = "My Universal Auth"
+app_publisher = "Tirath"
+app_description = "OAuth for Multiple Sites"
+app_email = "test@example.com"
+app_license = "MIT"
+app_version = "0.0.1"
+
+# Fixtures for OAuth Client
+fixtures = [
+    {
+        "dt": "OAuth Client",
+        "filters": [["app_name", "=", "SPS Commerce App"]]
+    }
+]
